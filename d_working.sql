317ee538bf082f7e4756e109a0d8e586909b507b
janlely <janlely@163.com>
2017-07-18 16:48:26 +0800

    add toto

---
+++ d_working.sql
@@ -0,0 +1,66 @@
+-- MySQL dump 10.13  Distrib 5.7.17, for Linux (x86_64)
+--
+-- Host: localhost    Database: jay_work
+-- ------------------------------------------------------
+-- Server version	5.7.17-0ubuntu0.16.04.1
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `t_line`
+--
+
+DROP DATABASE IF EXISTS `d_working`;
+CREATE DATABASE `d_working`;
+USE `d_working`;
+
+DROP TABLE IF EXISTS `t_line`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `t_line` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `insertion` int(11) NOT NULL,
+  `detletion` int(11) NOT NULL,
+  `detail` varchar(10000) NOT NULL,
+  `created` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `modified` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `t_todo`
+--
+
+DROP TABLE IF EXISTS `t_todo`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `t_todo` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `content` varchar(500) CHARACTER SET utf8 NOT NULL,
+  `created` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `modified` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  `status` tinyint(4) NOT NULL DEFAULT '0' COMMENT '0: 未完成, 1:已完成',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2017-03-06 19:46:29
---
+++ manage.py
@@ -0,0 +1,73 @@
+import os
+import subprocess
+
+def show_todo(conn):
+    cursor = conn.cursor()
+    query = ("SELECT id, content FROM t_todo WHERE status = 0")
+    cursor.execute(query)
+    print("work to do:")
+    for (id, content) in cursor:
+        print(" " * 4 + "{}: {}".format(id, content))
+    cursor.close()
+
+def show_done(conn):
+    cursor = conn.cursor()
+    query = ("SELECT id, content FROM t_todo WHERE status = 1")
+    cursor.execute(query)
+    print("work have done:")
+    for (id, content) in cursor:
+        print(" " * 4 + "{}: {}".format(id, content))
+    cursor.close()
+
+def add_todo(conn, add):
+    cursor = conn.cursor()
+    insert = ("INSERT INTO t_todo (content) VALUES ('{}')".format(add))
+    cursor.execute(insert)
+    print("add todo success")
+    conn.commit()
+    cursor.close()
+
+def kill_todo(conn, kill):
+    cursor = conn.cursor()
+    update = ("UPDATE t_todo SET status = 1 WHERE id = {}".format(kill))
+    cursor.execute(update)
+    conn.commit()
+    print("kill todo success")
+    cursor.close()
+
+
+def commit_diff_stat(conn, dirs):
+    cursor = conn.cursor()
+    for dir in dirs:
+        print(dir)
+        os.chdir(dir)
+        svnst = 'svn st'
+        svnstout = subprocess.getoutput(svnst)
+        if len(svnstout) == 0:
+            print("no code change to commit")
+            continue
+        cmd = 'svn diff --diff-cmd diff | diffstat'
+        out = subprocess.getoutput(cmd)
+        try:
+            insertion = out.split('\n')[-1:][0].split(',')[1].split(' ')[1]
+            deletion = out.split('\n')[-1:][0].split(',')[2].split(' ')[1]
+        except Exception:
+            return
+        detail = out
+        insert = ("INSERT INTO t_line (insertion, deletion, detail) VALUES (%s, %s, %s)")
+        cursor.execute(insert, (insertion, deletion, detail))
+        print("count code success, path: {}".format(dir))
+        print("detail: \n")
+        print(out)
+    conn.commit()
+    cursor.close
+
+def count_line_today(conn):
+    cursor = conn.cursor()
+    query = ("select sum(insertion) as insertions, sum(deletion) as deletions "
+             "from t_line where created > date_sub(now(), interval 1 day)")
+    cursor.execute(query)
+    for (insertions, deletions) in cursor:
+        print("today's work")
+        print(' ' * 4 + "insertions: {}, deletions: {}".format(insertions, deletions))
+
