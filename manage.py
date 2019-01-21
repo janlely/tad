e0360fb7708274f63622ffb69eb014c3e246171f
jinjunjie <jinjunjie@cmhi.chinamobile.com>
2019-01-21 17:26:43 +0800

    添加分类

--- manage.py
+++ manage.py
@@ -9,42 +9,67 @@ def getMonStartTime():
     tt = time.mktime(t)
     return tt - days_to_mon * 86400
 
-def show_todo(conn):
+def show_todo(conn, show_all):
     cursor = conn.cursor()
-    query = ("SELECT id, content FROM t_todo WHERE status = 0")
+    query = '''
+        SELECT t2.id, t2.content, t1.name
+        FROM t_topic t1
+        LEFT JOIN t_todo t2
+        ON t1.id = t2.topic_id
+        WHERE t1.current = 1
+        AND t2.status = 0
+    '''
+    if show_all:
+        query = '''
+            SELECT t2.id, t2.content, t1.name
+            FROM t_topic t1
+            LEFT JOIN t_todo t2
+            ON t1.id = t2.topic_id
+            WHERE t2.status = 0
+        '''
     cursor.execute(query)
     print("work to do:")
-    for (id, content) in cursor:
+    for (id, content, name) in cursor:
         content = content.replace('\n', '\n        ')
-        print(" " * 4 + "{}: {}".format(id, content))
+        print(" " * 4 + "{}: {}\n        {}".format(id, name, content))
     cursor.close()
 
 def show_done(conn):
     cursor = conn.cursor()
-    query = ("SELECT id, content, answer, modified FROM t_todo WHERE status = 1 ORDER BY modified")
+    query = '''
+        SELECT t1.id, t1.content, t1.answer, t1.modified, IFNULL(t2.name, 'NULL')
+        FROM t_todo t1
+        LEFT JOIN t_topic t2
+        ON t1.topic_id = t2.id
+        WHERE t1.status = 1 ORDER BY modified
+    '''
     cursor.execute(query)
     print("work have done:")
     t = int(getMonStartTime())
     flag = False
-    for (id, content, answer, modified) in cursor:
+    for (id, content, answer, modified, topic) in cursor:
         if(not flag and int(modified.timestamp()) > t):
             print("    DONE THIS WEEK:")
             flag = not flag
         answer = answer.replace('\n', '\n        ')
-        print(" " * 4 + "{}: {}".format(id, content))
+        print(" " * 4 + "{}: {}\n        {}".format(id, topic, content))
         print("        {}".format(answer))
     cursor.close()
 
 def add_todo(conn):
+    cursor = conn.cursor()
     EDITOR = os.environ.get('EDITOR','vim')
     file_name = '/tmp/question';
+    cursor.execute('SELECT id FROM t_topic WHERE current = 1')
+    current_topic_id = 0
+    for topic_id, in cursor:
+        current_topic_id = topic_id
     subprocess.call([EDITOR, file_name])
     with open(file_name, 'r') as f:
         question = f.read()
     if len(question.strip()) <= 0:
         return
-    cursor = conn.cursor()
-    insert = ("INSERT INTO t_todo (content) VALUES ('{}')".format(question))
+    insert = "INSERT INTO t_todo (content, topic_id) VALUES ('{}', {})".format(question, current_topic_id)
     cursor.execute(insert)
     print("add todo success")
     conn.commit()
@@ -84,6 +109,30 @@ def kill_todo(conn, kill):
     # subprocess.getoutput("sed -ie '2,$d' /tmp/answer")
     cursor.close()
 
+def list_topic(conn):
+    cursor = conn.cursor()
+    query = "SELECT name, current FROM t_topic WHERE enable = 1"
+    cursor.execute(query)
+    for topic, current in cursor:
+        if current == 1:
+            print("{}(current)".format(topic))
+        else:
+            print(topic)
+    cursor.close()
+
+def create_topic(conn, topic):
+    cursor = conn.cursor()
+    insert = "INSERT INTO t_topic (name) VALUES ({})".format(topic)
+    cursor.execute(insert)
+    conn.commit()
+    cursor.close()
+
+def switch_topic(conn, topic):
+    cursor = conn.cursor()
+    cursor.execute('UPDATE t_topic SET current = 0')
+    cursor.execute('UPDATE t_topic SET current = 1 WHERE name = "{}"'.format(topic))
+    conn.commit()
+    cursor.close()
 
 def commit_diff_stat(conn, dirs):
     cursor = conn.cursor()
--- readme.md
+++ readme.md
@@ -5,11 +5,15 @@
 ## Usage: tad [OPTIONS]
 ```
 Options:
--s, --show-todo    show todo list
--d, --show-done    show done list
--a, --add TEXT     add new todo
--k, --kill TEXT    kill todo by id
--m, --commit TEXT  commit svn diff stats in specific dirs
--c, --count        count how many lines of code you have writen today
---help             Show this message and exit.
+  -s, --show-todo                 show todo list
+  -o, --show-all                  show todo list
+  -d, --show-done                 show done list
+  -a, --add-todo                  add new todo
+  -e, --edit-todo TEXT            edit a todo
+  -k, --kill TEXT                 kill todo by id
+  -l, --list-topic                list all topics
+  -c, --create-topic TEXT         create a topic
+  -w, --switch-current-topic TEXT
+                                  switch current topic
+  --help                          Show this message and exit.
 ```
